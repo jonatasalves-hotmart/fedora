f2351aa9533f755bb3bf5a918bf4a32d1bd61c86
Jônatas Renan <40806339+jonatasalves-hotmart@users.noreply.github.com>
2018-12-19 12:10:33 -0200

    Update 09-zsh-terraform.sh

--- fedora29-post-installation/09-zsh-terraform.sh
+++ fedora29-post-installation/09-zsh-terraform.sh
@@ -12,8 +12,8 @@ if ! grep "terraform" ~/.oh-my-zsh/custom/themes/powerlevel9k/powerlevel9k.zsh-t
 # Add terraform workspace in prompt when needed 
 prompt_terraform() {
   if [[ -d .terraform ]]; then
-    local tf_workspace="$(tf_prompt_info | sed "s/\[//g" | sed "s/\]//g")"
-    "$1_prompt_segment" "$0" "$2" "black" "red" "$tf_workspace" "TERRAFORM_ICON"
+    local tf_workspace="\$(tf_prompt_info | sed "s/\[//g" | sed "s/\]//g")"
+    "\$1_prompt_segment" "\$0" "\$2" "black" "red" "\$tf_workspace" "TERRAFORM_ICON"
   fi
 }
 EOF
